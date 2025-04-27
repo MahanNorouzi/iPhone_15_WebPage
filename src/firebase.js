f6c2404f8cdc25203e5b17a46c1564e2f060118d
mahan <mahannorouzi53@gmail.com>
2025-04-27 14:38:14 +0330

    fixed api

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "apple-ea694.firebaseapp.com",
-  projectId: "apple-ea694",
-  storageBucket: "apple-ea694.appspot.com",
-  messagingSenderId: "593512790933",
-  appId: "1:593512790933:web:f8d179c80a7f5b3c04949c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+
+export const auth = getAuth(app);
